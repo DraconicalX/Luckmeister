b2c4954a7e288da8487ac0cc7ebc399c5055143e
DraconicalX <tonchen@nuevaschool.org>
2026-01-22 13:47:40 -0800

    Create Item.js

---
+++ luckmeister/js/item/Item.js
@@ -0,0 +1,16 @@
+class Item{
+  constructor(){
+    this.max = 9999;
+    this.height = 32;
+    this.width = 32;
+    this.invwidth = 24;
+    this.invheight = 24;
+    this.voidTime = 0;
+    this.armor = 0;
+    this.weapon = 0;
+    this.quest = 0;
+  }
+  stats(){
+    // override
+  }
+}
