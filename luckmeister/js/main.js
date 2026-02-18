5f3a3c8ef604ea6e0626cac413662fddd7a218a2
DraconicalX <tonchen@nuevaschool.org>
2026-02-17 19:45:21 -0800

    Update main.js

--- luckmeister/js/main.js
+++ luckmeister/js/main.js
@@ -10,6 +10,8 @@ function setup(){
   test = new Item();
 }
 function draw(){
+  background(255, 0, 0);
+  fill(0,255,0)
   rect(0,0,100,100)
   image(test.icon,0,0,100,100)
   image(tester,0,0,100,100)
