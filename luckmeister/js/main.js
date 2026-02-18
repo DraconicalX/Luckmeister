b11abc1c03f40132565504dd9b6b2f1b5f45a3c9
DraconicalX <tonchen@nuevaschool.org>
2026-02-17 18:05:25 -0800

    Update main.js

--- luckmeister/js/main.js
+++ luckmeister/js/main.js
@@ -5,8 +5,11 @@ function setup(){
   let cnv = createCanvas(windowWidth, windowHeight);
   
   cnv.parent(document.querySelector('main'));
+  let test = new Item();
 }
 function draw(){
   fill(255, 0, 0)
   rect(0,0,400,400)
+  image(test.icon,0,0,100,100)
+  
 }
