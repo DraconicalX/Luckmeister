b8706c385ad3e0d4cbfeb2c7f60b828478f1fcf2
DraconicalX <tonchen@nuevaschool.org>
2026-02-17 19:47:15 -0800

    Update main.js

--- luckmeister/js/main.js
+++ luckmeister/js/main.js
@@ -13,7 +13,7 @@ function draw(){
   background(255, 0, 0);
   fill(0,255,0)
   rect(0,0,100,100)
-  image(test.icon,0,0,100,100)
-  image(tester,0,0,100,100)
-  image(icon,0,0,100,100)
+  // image(test.icon,0,0,100,100)
+  // image(tester,0,0,100,100)
+  // image(icon,0,0,100,100)
 }
