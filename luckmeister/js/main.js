92547d36d717194fa3c84a096a88db1a68ce0d37
DraconicalX <tonchen@nuevaschool.org>
2026-02-17 20:37:07 -0800

    Update main.js

--- luckmeister/js/main.js
+++ luckmeister/js/main.js
@@ -7,14 +7,11 @@ function setup(){
   console.log("Setup has started!");
   let cnv = createCanvas(windowWidth, windowHeight);
   
-  // cnv.parent(document.querySelector('main'));
-  // test = new Item();
+  cnv.parent(document.querySelector('main'));
+  test = new Item();
 }
 function draw(){
   background(100);
   fill(0,255,0)
-  rect(0,0,100,100)
-  // image(test.icon,0,0,100,100)
-  // image(tester,0,0,100,100)
-  // image(icon,0,0,100,100)
+  image(test.icon,0,0,100,100)
 }
