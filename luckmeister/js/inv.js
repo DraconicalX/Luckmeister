20a3ed9d5796c4e65cf7e616406c09007b8c6162
DraconicalX <tonchen@nuevaschool.org>
2026-02-18 21:14:02 -0800

    Create inv.js

---
+++ luckmeister/js/inv.js
@@ -0,0 +1,9 @@
+class inv{
+  constructor(invSlots){
+    this.slotCount = invSlots
+    this.slot = loadImage("luckmeister/assets/invSlot.png")
+    for(i = 0; i<invSlots; i++){
+      image(slot,i*width/invSlots,0,width/invSlots,width/invSlots)
+    }
+  }
+}
