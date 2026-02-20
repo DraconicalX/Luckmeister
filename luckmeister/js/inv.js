d478b8afacbe394799ca0a7db4126ccf67819c9a
DraconicalX <tonchen@nuevaschool.org>
2026-02-20 11:10:27 -0800

    Update inv.js

--- luckmeister/js/inv.js
+++ luckmeister/js/inv.js
@@ -1,9 +1,27 @@
 class inventory{
   constructor(invSlots){
     this.slotCount = invSlots
-    this.slot = loadImage("luckmeister/assets/invSlot.png")
+    // this.slot = loadImage("luckmeister/assets/invSlot.png")
+    this.slots = []
     for(var i = 0; i<this.slotCount; i++){
-      image(this.slot,i*width/this.slotCount,0,width/this.slotCount,width/this.slotCount)
+      // image(this.slot,i*width/this.slotCount,0,width/this.slotCount,width/this.slotCount)
+      this.slots.push(new )
     }
+  }
+}
+class inventorySlot{
+  constructor(id){
+    this.slotNum = id;
+    this.item = null;
+    this.x = id*width/this.slotCount
+    this.y = 0
+  }
+  getXY(){
+    this.x = i*width/this.slotCount
+    this.y = 0
+  }
+  addItem(item){
+    item.x = this.x
+    item.y = this.y
   }
 }
